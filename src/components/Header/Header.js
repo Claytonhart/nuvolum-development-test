8eb7ca360e8dd4acaf888144225244a4fad992e5
Clayton Hart <claytonhart123@yahoo.com>
2022-10-21 20:03:40 -0700

    header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/macro';
 const Header = () => {
   return (
     <Wrapper>
-      <Heading>Heading</Heading>
+      <Heading>Modern Clothing</Heading>
       <Description>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
         architecto neque illum eius error, totam mollitia reiciendis officiis
@@ -16,10 +16,12 @@ const Header = () => {
 
 const Wrapper = styled.header`
   margin: 0 auto;
-  max-width: 50%;
+  max-width: 1000px;
+  padding: 32px;
 `;
 
 const Heading = styled.h1`
+  text-align: center;
   font-size: 3.5rem;
 `;
 
